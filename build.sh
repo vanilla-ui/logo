e2de861e0384e9246ff3abded30afa0dcd4cb82a
IntPtr <lxy.lixiaoyan@gmail.com>
2017-02-21 16:15:03 +0800

    add vanipack logo

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 inkscape -w 1000 -h 1000 -e logo.png logo.svg
+inkscape -w 1920 -h 960 -e vanipack/logo.png vanipack/logo.svg
 inkscape -w 3200 -h 960 -e vanilla-make/logo.png vanilla-make/logo.svg
